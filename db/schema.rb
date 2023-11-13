e265fdbd77b8f076597ff4c7185149a478aa6a34
ANTOINE1128 <makdessyantoine@gmail.com>
2023-11-13 17:00:43 +0200

    add models and migration files for food and recipe-food

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_13_140708) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_124736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.decimal "price", default: "0.0"
+    t.integer "quantity", default: 0
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_foods_on_name"
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.bigint "food_id"
+    t.integer "quantity", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_13_140708) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
 end
