4415a6a24af495187ee25bbdd11e60e426bd1b22
wolterbwambale <benplaan1@gmail.com>
2023-11-13 19:14:09 +0300

    add recipe mode and validations for the attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_12_124736) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_13_155155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_124736) do
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
   end
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "preparationTime"
+    t.string "cookTime"
+    t.text "description"
+    t.boolean "public"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -42,4 +53,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_124736) do
 
   add_foreign_key "foods", "users"
   add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipes", "users"
 end
