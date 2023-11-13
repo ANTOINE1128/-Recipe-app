a3a84cd5c0301d182f0cc1de5418f8494ba5bdb2
wolterbwambale <benplaan1@gmail.com>
2023-11-13 19:12:19 +0300

    add recipe model

---
+++ app/models/recipe.rb
@@ -0,0 +1,9 @@
+class Recipe < ApplicationRecord
+  belongs_to :user
+
+  validates :preparationTime, presence: true
+  validates :cookTime, presence: true
+  validates :description, presence: true, length: { minimum: 250 } 
+  validates :public, inclusion: { in: [true, false] }
+  validates :user, presence: true
+end
