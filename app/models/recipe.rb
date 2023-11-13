4419d887687095467cdb524d0b104145de738353
wolterbwambale <benplaan1@gmail.com>
2023-11-13 19:22:59 +0300

    add recipe mode

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
 
   validates :preparationTime, presence: true
   validates :cookTime, presence: true
-  validates :description, presence: true, length: { minimum: 250 } 
+  validates :description, presence: true, length: { minimum: 250 }
   validates :public, inclusion: { in: [true, false] }
   validates :user, presence: true
 end
